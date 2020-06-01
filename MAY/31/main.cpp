7835cb41ae1ed251e677a3516e9078c49724d5f4
Pranjal Walia <pranjalwalia77@gmail.com>
2020-06-02 02:47:45 +0530

    starting june

---
+++ MAY/31/main.cpp
@@ -0,0 +1,153 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii              pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define pw(b,p)         pow(b,p) + 0.1
+#define __              ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define rep(i,a,b) 		for(int i=a;i<b;i++)
+#define repb(i,a,b) 	for(int i=a;i>=b;i--)
+#define endl			"\n"
+#define m(a)		    memset(a,0,sizeof(a))
+#define all(x)                          begin(x), end(x)
+
+void fastscan(int &x){
+bool neg = false;register int c;
+x = 0;c = getchar();
+if(c=='-'){neg = true;c = getchar();}
+for(; (c>47 && c<58); c=getchar()){x = x *10 + c - 48;}
+if(neg){x *= -1;}}
+
+void file(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt" , "r" , stdin);
+	freopen("output.txt" , "w" , stdout);
+	#endif
+}
+
+int bs(int a[] , int s , int e, int x){
+	if(s<=e){
+		int m = (s+e)/2;
+		if(a[m]==x)
+			return m;
+		else if(a[m] > x)
+			return bs(a,s,m-1,x);
+		else return bs(a,m+1,e,x);
+	}
+	return -1;
+}
+
+#define tr(x) cout<<x<<endl;
+#define tr2(x,y) cout<<x<<" "<<y<<endl;
+#define tr3(x,y,z) cout<<x<<" "<<y<<" "<<z<<endl;
+#define tr4(w,x,y,z) cout<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
+#define tr5(v,w,x,y,z) cout<<v<<" "<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
+#define tr6(u,v,w,x,y,z) cout<<u<<" "<<v<<" "<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
+
+#define sd(x) cin >> x;
+#define sd2(x,y) cin >> x >> y;
+#define sd3(x,y,z) cin >> x >> y >> z;
+#define sd4(w,x,y,z) cin >> w >> x >> y >> z;
+
+bool is_pal(string s) {return equal(all(s), s.rbegin());}
+
+int mypow(int a, int b){        //(logn) fast exponentiation
+    int res = 1;            // call int x = mypow(2,5);
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a)%mod;
+        a = (a * a)%mod;
+        b >>= 1;
+    }
+    return (res%mod);
+}
+
+int n,m,q;
+int vis[1001][1001];
+int dis[1001][1001];
+char g[1001][1001];
+
+int dx[] = {1 , 0 , -1 , 0};
+int dy[] = {0 , -1 , 0 , 1};
+
+bool isValid(int x , int y){
+	if(vis[x][y]) return false;
+	if(x<1 || x>n || y<1 || y>m) return false;
+	return true;
+}
+
+void bfs(int x , int y){
+	queue<pii>q;
+	q.push({x,y}); 
+	vis[x][y]=1;
+	dis[x][y]=0;
+	cout << "pushed source..." << endl;
+	while(!q.empty()){
+		int currx = q.front().ff;
+		int curry = q.front().ss;
+		q.pop();
+		cout << "popped succeess" << endl;
+		if(!vis[currx][curry]){
+			for(int i=0 ; i<4 ; i++){
+				int l = currx+dx[i]; 
+				int r = curry+dy[i];
+
+				if(isValid(l,r) && vis[l][r]==0 && g[l][r]=='O'){
+					q.push({l,r});
+					printf("pushed (%lld , %lld)..." , l , r);
+					vis[l][r]=1;
+					dis[l][r]=dis[currx][curry]+1;
+				}
+			}
+		}
+	}
+}
+
+int32_t main(){	
+	__;
+	file();
+	sd3(n,m,q);
+	rep(i , 0 ,n){
+		rep(j , 0 ,m){
+			cin >> g[i+1][j+1];
+		}
+	}
+	int x,y; sd2(x,y);
+	bfs(x,y);
+	while(q--){
+		int fx , fy; sd2(fx,fy);
+		if(fx==x && fy==y){
+			cout << 0 << endl; continue;
+		}
+		else if(dis[fx][fy]==0){
+			cout << -1 << endl; continue;
+		}
+		else cout << dis[fx][fy] << endl;
+	}
+	cout << endl;
+	rep(i , 1 ,n+1){
+		rep(j ,1 , m+1){
+			cout << dis[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
+
